01655ec3c189c568a652ef2e2e17b0ad7e9bc7bf
NormalB <uminarie@gmail.com>
2018-04-08 01:42:36 +0900

    20180408 #2112 보호필름

--- SWEA/2112.cpp
+++ SWEA/2112.cpp
@@ -1,113 +1,75 @@
 #include <cstdio>
-#include <queue>
+#include <cstring>
 
 int board[13][20];
 int copy_board[13][20];
+int zeros[20];
+int ones[20];
+bool visited[26];
 int T, D, W, K;
 int global_min;
+bool stop;
 
-class State {
-public:
-	int round;
-	bool visited[26] = {false, };
-
-	State(int r) {
-		round = r;
+void check(int m) {
+	//if K is 1, all pass
+	if (K == 1){
+		global_min = m;
+		stop = true;
+		return;
 	}
-
-	State* copy() {
-		State *ns = new State(round + 1);
-		for (int i = 0; i < 26; i++)
-			ns->visited[i] = visited[i];
-		return ns;
+	//make copy_board
+	memcpy(*copy_board, *board, 13*20*sizeof(int));
+	//make change to the board
+	for (int p = 0; p < D; p++) {
+		if (visited[p])
+			memcpy(*(copy_board+p), zeros, 20*sizeof(int));
 	}
-};
-
-std::queue<State*> Q;
-
-void clearQ(std::queue<State*> *someQueue)
-{
-	std::queue<State*> newQueue;
-	std::swap(*someQueue, newQueue);
+	for (int p = 13; p < D + 13; p++) {
+		if (visited[p])
+			memcpy(*(copy_board + (p-13)), ones, 20*sizeof(int));
+	}
+	//check pass or fail
+	bool pass = true;
+	for (int j = 0; j < W; j++) {
+		int max_streak = 0;
+		int streak = 1;
+		for (int i = 1; i < D; i++) {
+			if (copy_board[i][j] == copy_board[i - 1][j]) {
+				streak++;
+				if (max_streak < streak) max_streak = streak;
+			}
+			else {
+				streak = 1;
+			}
+		}
+		if (max_streak < K) {
+			pass = false;
+			break;
+		}
+	}
+	if (pass) {
+		global_min = m;
+		stop = true;
+	}
 }
 
-void bfs() {
-	State *s = new State(0);
-	Q.push(s);
-	while (!Q.empty()) {
-		State *ts = Q.front();
-		/*
-			printf("when round %d, visited: ", ts->round);
-			for (int i = 0; i < 26; i++)
-				printf("%d ", ts->visited[i]);
-			printf("\n");
-		*/
-		Q.pop();
-		//make copy board
-		for (int i = 0; i < D; i++) {
-			for (int j = 0; j < W; j++) {
-				copy_board[i][j] = board[i][j];
-			}
-		}
-		//make change to the board
-		for (int p = 0; p < D; p++) {
-			if (ts->visited[p]) {
-				for (int j = 0; j < W; j++) {
-					copy_board[p][j] = 0;
-				}
-			}
-		}
-		for (int p = 13; p < D + 13; p++) {
-			if (ts->visited[p]) {
-				for (int j = 0; j < W; j++) {
-					copy_board[p-13][j] = 1;
-				}
-			}
-		}
-		//check pass or fail
-		bool pass = true;
-		for (int j = 0; j < W; j++) {
-			int max_streak = 0;
-			int streak = 1;
-			for (int i = 1; i < D; i++) {
-				if (copy_board[i][j] == copy_board[i - 1][j]) {
-					streak++;
-					if (max_streak < streak) max_streak = streak;
-				}
-				else {
-					streak = 1;
-				}
-			}
-			if (max_streak < K) {
-				pass = false;
-				break;
-			}
-		}
-		if (pass){
-			global_min = ts->round;
-			return;
-		}
-		//change row by row
-		for (int i = 0; i < D; i++) {
-			if (!ts->visited[i]) {
-				State *ns = ts->copy();
-				ns->visited[i] = true;
-				Q.push(ns);
-			}
-		}
-		for (int i = 13; i < D+13; i++) {
-			if (!ts->visited[i]) {
-				State *ns = ts->copy();
-				ns->visited[i] = true;
-				Q.push(ns);
-			}
-		}
-		delete ts;
+void comb(int n, int m, int selected, int now) {
+	if (selected == m) {
+		check(m);
+		return;
+	}
+	for (int i = now+1; i < n && !stop; i++) {
+		visited[i] = true;
+		comb(n, m, selected + 1, i);
+		visited[i] = false;
 	}
 }
 
 int main(void) {
 	/*
+	//init ones
+	for (int i = 0; i < 20; i++)
+	ones[i] = 1;
 	scanf("%d", &T);
 	for (int test = 1; test <= T; test++) {
 		scanf("%d%d%d", &D, &W, &K);
@@ -116,11 +78,16 @@ int main(void) {
 				scanf("%d", &board[i][j]);
 			}
 		}
-		bfs();
+		stop = false;
+		for (int i = 0; i <= 26 && !stop; i++) {
+			comb(26, i, 0, -1);
+		}
 		printf("#%d %d\n", test, global_min);
-		clearQ(Q);
 	}
 	*/
+	//init ones
+	for (int i = 0; i < 20; i++)
+		ones[i] = 1;
 	FILE *fp = fopen("c:/users/bjg01/desktop/test.txt", "r");
 	fscanf(fp, "%d", &T);
 	for (int test = 1; test <= T; test++) {
@@ -130,9 +97,11 @@ int main(void) {
 				fscanf(fp, "%d", &board[i][j]);
 			}
 		}
-		bfs();
+		stop = false;
+		for (int i = 0; i <= 26 && !stop; i++) {
+			comb(26, i, 0, -1);
+		}
 		printf("#%d %d\n", test, global_min);
-		clearQ(&Q);
 	}
 	fclose(fp);
 }
